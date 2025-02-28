71e4fedc51b1f89e8f1f8fd59a1b01c392e854bd
Luis Gabriel Venegas Saucedo <83895876+LuisGabriel112@users.noreply.github.com>
2025-02-28 11:12:09 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,14 +2,21 @@ console.log('Hello, world!');
 // Definir un template
 
 const templateCasa = (titulo, ubicacion, habitaciones, baños, tamaño, garages, imagen ) => {
-    return `<div class="casa">
+    return `<div data-aos="fade-up" data-aos-duration="1000" class="casa">
+    <div class="casaT">
         <h2>${titulo}</h2>
-        <p>Ubicación: ${ubicacion}</p>
-        <p>Habitaciones: ${habitaciones}</p>
-        <p>Baños: ${baños}</p>
-        <p>Tamaño: ${tamaño}</p>
-        <p>Garages: ${garages}</p>
-        <img src="${imagen}" alt="Imagen de la casa">
+        <p class="ubiP"><img class="ubi_img" src="img/assets/location-pin-svgrepo-com.svg" alt="">Ubicación: ${ubicacion}</p>
+        <div class="caracteristicas">
+        <p class="div1"><img class="bed_img" src="img/assets/bed-svgrepo-com (1).svg" alt="">Habitaciones: ${habitaciones}</p>
+        <p class="div2"> <img class="bath_img" src="img/assets/bath-tube-svgrepo-com.svg" alt="">Baños: ${baños}</p>
+        <p class="div3"> <img class="rule_img" src="img/assets/ruler-svgrepo-com.svg" alt="">Tamaño: ${tamaño}</p>
+        <p class="div4"> <img class="garage_img" src="img/assets/garage-property-svgrepo-com.svg" alt="">Garages: ${garages}</p>
+        <a href="propiedades.html" class="div5 button-86" role="button">Ver más</a>
+        </div>
+    </div>
+    <div class="casaI">
+        <img class="casa_img" src="${imagen}" alt="Imagen de la casa">
+    </div>
     </div>`;
 }
 
